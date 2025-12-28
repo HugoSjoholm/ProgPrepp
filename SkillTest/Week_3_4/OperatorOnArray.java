d3866197c54d93d7a18fc3f22e633cbb9c42d17c
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-12-29 00:51:49 +0100

    Update OperatorOnArray.java

--- SkillTest/Week_3_4/OperatorOnArray.java
+++ SkillTest/Week_3_4/OperatorOnArray.java
@@ -1,13 +1,42 @@
 public class OperatorOnArray {
     public static void main(String[] args) {
-        int input = Integer.parseInt(args[0]);
+        int number = Integer.parseInt(args[0]);
+        char operator = args[1].charAt(0);
 
         int[] list = { 0, -12, 200, 67, 0, 0, 1, 200 };
         printArray(list);
 
-        System.out.println("Do the operations!");
-        for (int i = 0; i < list.length; i++) {
-            list[i] = list[i] + input;
+
+        switch (operator) {
+            case '+':
+                System.out.println("Do the operations!");
+                for (int i = 0; i < list.length; i++) {
+                    list[i] = list[i] + number;
+                }                
+                break;
+            case '-':
+                System.out.println("Do the operations!");
+                for (int i = 0; i < list.length; i++) {
+                    list[i] = list[i] - number;
+                }
+                break;
+            case '*':
+                System.out.println("Do the operations!");
+                for (int i = 0; i < list.length; i++) {
+                    list[i] = list[i] * number;
+                }
+                break;
+            case '/':
+                System.out.println("Do the operations!");
+                for (int i = 0; i < list.length; i++) {
+                    list[i] = list[i] / number;
+                }
+                break;
+
+            default:
+                System.out.println("Fel operator! Försök igen med någon av dessa: +  -  *  /");
+                System.out.println("Exempel: java OperatorOnArray.java 10 *");
+                break;
         }
 
         printArray(list);
