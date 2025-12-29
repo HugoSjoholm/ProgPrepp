c2b282100f5cc79c3e5ccef86b012bae098de0cb
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2025-12-29 02:43:27 +0100

    Created PrintArrays (part 1 and part 2)

---
+++ SkillTest/Week_3_4/PrintArraysPart1.java
@@ -0,0 +1,23 @@
+public class PrintArraysPart1 {
+    public static void main(String[] args) {
+        int[] intList = { 0, -12, 200, 67, 0, 0, 1, 200 };
+        int[] randomList = new int[20];
+        
+        for (int i = 0; i < randomList.length; i++) {
+            randomList[i] = (int)(Math.random()*100-50);
+        }
+
+        System.out.println(" * Print int list! * ");
+        PrintArray(intList);
+        
+        System.out.println(" * Print random int list! * ");
+        PrintArray(randomList);
+
+    }
+    public static void PrintArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println(); 
+    }
+}
