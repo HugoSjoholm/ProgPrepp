f2bf506f14bcc79caeab506fcecd171b80aa4529
Hugo Sjöholm <111504314+HugoSjoholm@users.noreply.github.com>
2026-01-13 20:48:07 +0100

    tmp

--- SkillTest/Week_5_6/facit/ComplexMath.java
+++ SkillTest/Week_5_6/facit/ComplexMath.java
@@ -1,10 +1,29 @@
 public class ComplexMath {
     public static void main(String[] args) {
         //this program and the class ComplexNum.java is apart of the same assigment. 
+        
+        //Make four constructors in the ComplexNum class. 
+        // * (float real, float imaginary) 
+        // * (float real) 
+        // * (int real, int imaginary)
+        // * (int real) 
+        //Make two complex numbers, num1 and num2, num1 should be defined with ints. num2 should be defined with floats (kinda decimals)
+        //Print both x and y parts of num1 and num2 using dot notations (for example array.length)
         ComplexNum num1 = new ComplexNum(1, 2);
         ComplexNum num2 = new ComplexNum(1.5f, -1.447f);
 
+        //System.out.println(num1.x + " " + num1.y);
+        //System.out.println(num2.x + " " + num2.y);
         
+        //In the ComplexNum class  make the variables re and im private. 
+        // * private float re;
+        // * private float im;
+        //Then try to print num1 and num2 using the same way you did before. Does your previous code still work?
+
+        //In the ComplexNum class make get and set methods for both re and im. getReal(), setReal(), getImaginary(), setImaginary()
+        //Then try to print both num1 and num2 
+        
+
 
 
 
